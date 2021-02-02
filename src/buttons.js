9f4d362cb958d59eaeee713a2682f0ed43649e3c
mariosknl <mariosknl@outlook.com>
2021-02-02 14:42:57 +0200

    interacting with the UI

--- src/buttons.js
+++ src/buttons.js
@@ -2,12 +2,11 @@ import { ICONS } from "./constants";
 
 const toggleHighlighted = (icon, show) =>
   document
-    .querySelector(`${ICONS[icon]}-icon`)
+    .querySelector(`.${ICONS[icon]}-icon`)
     .classList.toggle("highlighted", show);
 
 export default function initButtons(handleUserAction) {
   let selectedIcon = 0;
-
   function buttonClick({ target }) {
     if (target.classList.contains("left-btn")) {
       toggleHighlighted(selectedIcon, false);
@@ -15,7 +14,8 @@ export default function initButtons(handleUserAction) {
       toggleHighlighted(selectedIcon, true);
     } else if (target.classList.contains("right-btn")) {
       toggleHighlighted(selectedIcon, false);
-      selectedIcon = (1 + selectedIcon) & ICONS.length;
+      selectedIcon = (1 + selectedIcon) % ICONS.length;
+      toggleHighlighted(selectedIcon, true);
     } else {
       handleUserAction(ICONS[selectedIcon]);
     }
